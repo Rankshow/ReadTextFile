f4f241eb81a2e72bbd41a16604eae39e2932741d
Rankshow <uwaomaobinna20@gmail.com>
2023-09-20 22:17:41 +0100

    Read from file in local system

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace ReadTextFile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String line;
+        try
+        {
+          StreamReader sr = new StreamReader(@"C:\Data\Programmers.txt");
+          line = sr.ReadLine();
+          
+          while(line != null)
+          {
+            Console.WriteLine(line);
+            line = sr.ReadLine();
+          }
+          sr.Close();
+          Console.WriteLine(line);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Sorry!! we  :" + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Thanks, at this moment we have to executing the code");
+        }
+    }
+}
